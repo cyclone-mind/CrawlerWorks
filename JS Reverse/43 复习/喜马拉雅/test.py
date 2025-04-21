e8633ee05310a6a05a3beac55b6a2d931929dc87
shouyu <shouyu@gmail.com>
2025-04-21 15:24:33 +0800

    Stop tracking specified folders

--- JS Reverse/43 复习/喜马拉雅/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import execjs
-
-account = '13498556932',
-password = 'kuis'
-
-
-enc_account = execjs.compile(open('./account_pwd.js', encoding='utf-8').read()).call('get_account', account)
-print(enc_account)
